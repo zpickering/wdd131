79ed74cdc5ad3dffc5a8e3e9e1fef00d58d89630
zpickering <pic23009@byui.edu>
2024-09-21 21:51:12 -0600

    changes

---
+++ mission/mission.js
@@ -0,0 +1,24 @@
+// Select the dropdown element from the HTML
+const themeSelector = document.querySelector('#theme-selector');
+
+// Function to change the theme
+function changeTheme() {
+    const body = document.body; // Select the body element
+    const logo = document.getElementById('logo'); // Select the logo image
+
+    // Check the current value of the select element
+    if (themeSelector.value === 'dark') {
+        // Add the dark class to the body
+        body.classList.add('dark');
+        // Change the source of the logo to the white BYUI logo
+        logo.src = 'byui-logo_white.png'; // Path to the white logo
+    } else {
+        // Remove the dark class from the body
+        body.classList.remove('dark');
+        // Change the source of the logo to the blue logo
+        logo.src = 'byui-logo_blue.png'; // Path to the blue logo
+    }
+}
+
+// Add an event listener to the themeSelector element
+themeSelector.addEventListener('change', changeTheme);
---
+++ sandbox/running2.js
@@ -0,0 +1,13 @@
+const myApples = 4;
+const friendApples = 2;
+let total = myApples + friendApples;
+
+document.getElementById("myAppleElement").textContent = myApples;
+document.getElementById("friendAppleElement").textContent = friendApples;
+document.getElementById("totalApplesElement").textContent = total;
+
+
+
+
+
+
