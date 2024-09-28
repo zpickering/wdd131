1bb8ff6aaec7efee46a5e53364b72086a298e140
zpickering <pic23009@byui.edu>
2024-09-28 14:44:31 -0600

    cool pics

---
+++ coolpics/coolpics.js
@@ -0,0 +1,65 @@
+const menuButton = document.querySelector(".menu-button");
+console.log(menuButton);
+const gallery = document.querySelector('.gallery');
+
+
+function toggleMenu() {
+    console.log("Button clicked!");
+    const menuLinks = document.querySelectorAll(".links a");
+    menuLinks.forEach(link => {
+        link.classList.toggle("hide");
+    });
+}
+
+function handleResize() {
+    const menu = document.querySelectorAll(".links a");
+    if (window.innerWidth > 1000) {
+      menu.forEach(link => {
+        link.classList.remove("hide");
+      });
+    } else {
+      menu.forEach(link => {
+        link.classList.add("hide");
+      });
+    }
+  }
+
+function viewerTemplate(pic, alt) {
+  return `<div class="viewer">
+    <button class="close-viewer">X</button>
+    <img src="${pic}" alt="${alt}">
+    </div>`;
+}
+function viewHandler(event) {
+  // Create a variable to hold the element that was clicked on from event.target
+  const clickedElement = event.target;
+
+  // Get the src attribute from that element and 'split' it on the "-"
+  const imgSrc = clickedElement.getAttribute('src');
+  const imgSrcParts = imgSrc.split('-');
+
+  // Construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
+  const fullImgSrc = `${imgSrcParts[0]}-full.jpeg`;
+
+  // Insert the viewerTemplate into the top of the body element
+  const altText = clickedElement.getAttribute('alt'); // Get alt text from the clicked image
+  const modalHTML = viewerTemplate(fullImgSrc, altText);
+  document.body.insertAdjacentHTML('afterbegin', modalHTML); // Insert the modal at the top of the body
+
+  //Add a listener to the close button (X) that calls a function called closeViewer when clicked
+  const closeButton = document.querySelector('.close-viewer');
+  closeButton.addEventListener('click', closeViewer);
+}
+
+function closeViewer() {
+  const viewer = document.querySelector('.viewer');
+  if (viewer) {
+    viewer.remove();  // Remove the viewer div
+  }
+}
+
+
+handleResize();
+gallery.addEventListener('click', viewHandler);
+window.addEventListener("resize", handleResize);
+menuButton.addEventListener("click", toggleMenu);
---
+++ events/events.js
@@ -0,0 +1,72 @@
+let tasks = [];
+
+function taskTemplate(task) {
+  return `
+    <li ${task.completed ? 'class="strike"' : ""}>
+      <p>${task.detail}</p>
+      <div>
+        <span data-function="delete">❎</span>
+        <span data-function="complete">✅</span>
+      </div>
+    </li>`
+}
+
+function renderTasks(tasks) {
+  // get the list element from the DOM
+  const listElement = document.querySelector("#todoList");
+  listElement.innerHTML = "";
+  // loop through the tasks array. transform (map) each task object into the appropriate HTML to represent a to-do.
+  const html = tasks.map(taskTemplate).join("");
+  listElement.innerHTML = html;
+}
+
+function newTask() {
+  // get the value entered into the #todo input
+  const taskInput = document.querySelector("#todo");
+  const task = taskInput.value;
+  // add it to our arrays tasks
+  tasks.push({ detail: task, completed: false });
+
+  // render out the list
+  renderTasks(tasks);
+  
+  //clear the input box
+  taskInput.value = '';
+}
+
+function removeTask(taskElement) {
+  // Notice how we are using taskElement instead of document as our starting point?
+  // This will restrict our search to the element instead of searching the whole document.
+  tasks = tasks.filter(
+    (task) => task.detail != taskElement.querySelector('p').innerText
+  );
+  taskElement.remove();
+}
+
+function completeTask(taskElement) {
+  const taskIndex = tasks.findIndex(
+    (task) => task.detail === taskElement.querySelector('p').innerText
+  );
+  tasks[taskIndex].completed = tasks[taskIndex].completed ? false : true;
+  taskElement.classList.toggle("strike");
+  console.log(tasks);
+}
+
+function manageTasks(e) {
+  // did they click the delete or complete icon?
+  console.log(e.target);
+  const parent = e.target.closest("li");
+  if (e.target.dataset.action === "delete") {
+    removeTask(parent);
+  }
+  if (e.target.dataset.action === "complete") {
+    completeTask(parent);
+  }
+}
+
+// Add your event listeners here
+document.querySelector("#submitTask").addEventListener("click", newTask);
+document.querySelector("#todoList").addEventListener("click", manageTasks);
+
+// render  the initial list of tasks (if any) when the page loads
+renderTasks(tasks);
